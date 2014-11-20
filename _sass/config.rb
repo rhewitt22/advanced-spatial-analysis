3a5642d9518a9ce7dfb4edce386ae1dfd99bc12d
Roy Hewitt <royhewitt07@gmail.com>
2014-11-20 16:21:37 -0500

    Updated http_images_dir

--- _sass/config.rb
+++ _sass/config.rb
@@ -2,20 +2,23 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "http://rhewitt22.github.io/advanced-spatial-analysis/"
 css_dir = "../css"
 sass_dir = "./"
 images_dir = "../img"
 javascripts_dir = "../js"
 
+http_images_dir = "http://rhewitt22.github.io/advanced-spatial-analysis/img"
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
